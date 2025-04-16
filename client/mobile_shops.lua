bf2c264d25a16ed494890ce2bef793680e14c130
Vibrant <127861623+VibrantRP@users.noreply.github.com>
2025-04-16 17:40:03 +0100

    Fix: Add check to see if shops are being used

--- client/mobile_shops.lua
+++ client/mobile_shops.lua
@@ -1,45 +1,47 @@
-CreateThread(function()
-    Wait(10000)
-    local mobileShopInfo = lib.callback.await('ambience:server:GatherMobileShops', false)
+if Mobile.UseMobileShops then
+    CreateThread(function()
+        Wait(10000)
+        local mobileShopInfo = lib.callback.await('ambience:server:GatherMobileShops', false)
 
-    for k, v in pairs(mobileShopInfo) do
-        Wait(500)
-        local clientTarget = NetworkGetEntityFromNetworkId(v.clientEntity)
+        for k, v in pairs(mobileShopInfo) do
+            Wait(500)
+            local clientTarget = NetworkGetEntityFromNetworkId(v.clientEntity)
 
-        SetEntityInvincible(clientTarget, true)
-        SetBlockingOfNonTemporaryEvents(clientTarget, true)
+            SetEntityInvincible(clientTarget, true)
+            SetBlockingOfNonTemporaryEvents(clientTarget, true)
 
-        TriggerEvent('ambience:client:CreatePatrolRoute', clientTarget, v.shopData.Route)
+            TriggerEvent('ambience:client:CreatePatrolRoute', clientTarget, v.shopData.Route)
 
-        if v.shopData.StoreType == "Buying" then
-            exports.ox_target:addLocalEntity(clientTarget,  {
-                {
-                    label = "Buy "..k,
-                    icon = 'fa-solid fa-cart-shopping',
-                    name = k,
-                    onSelect = function()
-                        Wait(100)
+            if v.shopData.StoreType == "Buying" then
+                exports.ox_target:addLocalEntity(clientTarget,  {
+                    {
+                        label = "Buy "..k,
+                        icon = 'fa-solid fa-cart-shopping',
+                        name = k,
+                        onSelect = function()
+                            Wait(100)
 
-                        PlayPedAmbientSpeechNative(clientTarget, 'GENERIC_HI', 'Speech_Params_Force')
-                        exports.ox_inventory:openInventory('shop', { 
-                            type = k,
-                        })
-                    end,
-                }
-            })
-        elseif v.shopData.StoreType == "Selling" then
-            exports.ox_target:addLocalEntity(clientTarget,  {
-                {
-                    label = "Sell "..k,
-                    icon = 'fa-solid fa-sack-dollar',
-                    onSelect = function()
-                        Wait(100)
+                            PlayPedAmbientSpeechNative(clientTarget, 'GENERIC_HI', 'Speech_Params_Force')
+                            exports.ox_inventory:openInventory('shop', { 
+                                type = k,
+                            })
+                        end,
+                    }
+                })
+            elseif v.shopData.StoreType == "Selling" then
+                exports.ox_target:addLocalEntity(clientTarget,  {
+                    {
+                        label = "Sell "..k,
+                        icon = 'fa-solid fa-sack-dollar',
+                        onSelect = function()
+                            Wait(100)
 
-                        PlayPedAmbientSpeechNative(clientTarget, 'GENERIC_HI', 'Speech_Params_Force')
-                        exports.ox_inventory:openInventory('stash', k)
-                    end,
-                }
-            })
+                            PlayPedAmbientSpeechNative(clientTarget, 'GENERIC_HI', 'Speech_Params_Force')
+                            exports.ox_inventory:openInventory('stash', k)
+                        end,
+                    }
+                })
+            end
         end
-    end
-end)
+    end)
+end
